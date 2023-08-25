00040678aeb4e2213396d0059be6f173f18be63b
RodrigoMaga <rgn.nunnes@gmail.com>
2023-08-25 18:07:44 -0300

    exercicio2

--- src/main/java/org/example/exercicios/estruturascond/Exercicio2.java
+++ src/main/java/org/example/exercicios/estruturascond/Exercicio2.java
@@ -10,22 +10,20 @@ public class Exercicio2 {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        int code, amount, total;
-
         Product hotDog = new Product(1, 4.50);
         Product xSalad = new Product(2, 4.50);
         Product xBacon = new Product(3, 5.00);
-        Product simpleToast = new Product(4, 2.00);
+        Product toast = new Product(4, 2.00);
         Product soda = new Product(5, 1.50);
 
         System.out.print("Informe o código do item: ");
-        code = scanner.nextInt();
+        int code = scanner.nextInt();
 
         switch (code){
             case 1 -> System.out.printf("Total: %.2f", getTotal(hotDog));
             case 2 -> System.out.printf("Total: %.2f", getTotal(xSalad));
             case 3 -> System.out.printf("Total: %.2f", getTotal(xBacon));
-            case 4 -> System.out.printf("Total: %.2f", getTotal(simpleToast));
+            case 4 -> System.out.printf("Total: %.2f", getTotal(toast));
             case 5 -> System.out.printf("Total: %.2f", getTotal(soda));
         }
     }
