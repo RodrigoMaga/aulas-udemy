f4141d67eb32f0d42f20d01e4583c04596115236
RodrigoMaga <rgn.nunnes@gmail.com>
2023-08-28 17:28:18 -0300

    exercicio 4

---
+++ src/main/java/org/example/exercicios/estruturascond/Exercicio4.java
@@ -0,0 +1,13 @@
+package org.example.exercicios.estruturascond;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x, y;
+
+
+    }
+}
