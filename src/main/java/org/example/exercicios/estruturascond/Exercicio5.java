229e14bf4cd943f48fdc6e7a95b30f231cf5b43c
RodrigoMaga <rgn.nunnes@gmail.com>
2023-08-31 11:09:10 -0300

    exercicio imposto

---
+++ src/main/java/org/example/exercicios/estruturascond/Exercicio5.java
@@ -0,0 +1,34 @@
+package org.example.exercicios.estruturascond;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        double renda, impostoRenda;
+        System.out.print("Informe seu salário: ");
+        renda = scanner.nextDouble();
+
+        if (renda > 2000) {
+            if (renda <= 3000) {
+                impostoRenda = (renda - 2000) / 100 * 8;
+                System.out.printf("R$ %.2f", impostoRenda);
+            }
+            else if (renda <= 4500) {
+                impostoRenda = (renda - 3000) / 100 * 18;
+                impostoRenda += 80;
+                System.out.printf("R$ %.2f", impostoRenda);
+            }
+            else {
+                impostoRenda = (renda - 4500) / 100 * 28;
+                impostoRenda += 80 + ((1500 / 100) * 18);
+                System.out.printf("R$ %.2f", impostoRenda);
+            }
+        } else {
+            System.out.println("Insento");
+        }
+    }
+}
